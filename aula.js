74bb198e9402078e59c2f4f9235652439a2b207f
leticia <lleticialmeida9@gmail.com>
2022-11-25 10:47:58 -0300

    World Cup - Brazil X Switzerland

--- aula.js
+++ aula.js
@@ -7,6 +7,8 @@ http.createServer(function(req, res){
 res.writeHead(200, {'content-Type': 'text/html'});
 
 res.write("<h2> Brasil 2 X 0 Servia </h2> <br>");
+
+res.write("<h2> Brasil 2 X 1 Suíça </h2> <br>");
                                                 //substring:retorna caracteres do valor cadeia a partir da posição de caractere especificada por início 
 res.write('<p style=color:green>' + Date().substring(16,24)+'<br>'); //essa função pega a data e a hora completa
                                                 
