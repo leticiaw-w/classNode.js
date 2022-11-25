ba83ac82aac8a7959a13aa77a920be7730bb8de5
leticia <lleticialmeida9@gmail.com>
2022-11-25 10:43:13 -0300

    utilizando uma função - aula.js

--- aula.js
+++ aula.js
@@ -1,13 +1,17 @@
 var http = require('http');
 
+const dia = require('./biblioteca');
+
 http.createServer(function(req, res){
 
-res.writeHead(200, {'content-Type': 'text/html'})
+res.writeHead(200, {'content-Type': 'text/html'});
 
 res.write("<h2> Brasil 2 X 0 Servia </h2> <br>");
-
-res.write(Date().substring(16,24)+'<br> <br>'); //essa função pega a data e a hora completa
                                                 //substring:retorna caracteres do valor cadeia a partir da posição de caractere especificada por início 
+res.write('<p style=color:green>' + Date().substring(16,24)+'<br>'); //essa função pega a data e a hora completa
+                                                
+res.write(dia.diaDaSemana()+'</p> <br>')
+
 res.end("bom dia!"); //sempre usar para finalizar o programa
 
 }).listen(3000);
---
+++ biblioteca.js
@@ -0,0 +1,5 @@
+exports.diaDaSemana = function(){ //o nome é criado pelo programador
+
+    return Date().substring(0,3);
+
+};
