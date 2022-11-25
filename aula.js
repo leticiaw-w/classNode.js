6af733428f873133beaeac7ec5a742831b3de964
leticia <lleticialmeida9@gmail.com>
2022-11-25 10:15:51 -0300

    complemento do primeiro exemplo

--- aula.js
+++ aula.js
@@ -2,8 +2,12 @@ var http = require('http');
 
 http.createServer(function(req, res){
 
-res.write("Brasil 4 X 1 Servia \n");
+res.writeHead(200, {'content-Type': 'text/html'})
 
+res.write("<h2> Brasil 2 X 0 Servia </h2> <br>");
+
+res.write(Date().substring(16,24)+'<br> <br>'); //essa função pega a data e a hora completa
+                                                //substring:retorna caracteres do valor cadeia a partir da posição de caractere especificada por início 
 res.end("bom dia!"); //sempre usar para finalizar o programa
 
 }).listen(3000);
