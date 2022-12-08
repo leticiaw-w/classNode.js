2e3d0cbf55df7026a7739875011bdd3f740280bf
Leticia <lleticialmeida9@gmail.com>
2022-12-08 08:32:40 -0300

    mínimas atualizações do -inserindo data(chgd)

--- aula2.js
+++ aula2.js
@@ -4,10 +4,11 @@ let url = require('url');
 http.createServer(function (req, res){
     res.writeHead(200, {'content-Type': 'text/html'});
     res.write(req.url);
-    let q = url.parse(req.url, true).query;
+    let q = url.parse(req.url, true).query; //parse: cortar
     let txt = "<br>dia:" +q.dia+ "<br>mês:" +q.mes+ "<br> ano:" +q.ano;
     res.end(txt);
 
 }).listen(80);  
 
 //passando informação direto do site: http://localhost/?dia=2&mes=jan&ano=2021
+//se tiver mais de uma váriável, coloca &
